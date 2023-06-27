db9889e6a6575fd8a75d215178217414e7efcaae
Gilzamir Gomes <54516662+gilzamir18@users.noreply.github.com>
2023-06-27 00:44:45 -0300

    Fix config.

--- mundowumpus/setup.py
+++ mundowumpus/setup.py
@@ -4,5 +4,5 @@ setup(name='mundowumpus',
         version='0.0.1',
         packages=['mundowumpus'],
         package_dir={'mundowumpus': 'src'},
-        install_requires=['gym>=0.10.5',  'numpy'],
+        install_requires=['gym>=0.26.2',  'numpy'],
         py_modules=[])
--- mundowumpus/src/wumpus.py
+++ mundowumpus/src/wumpus.py
@@ -1,5 +1,5 @@
 import gym
-import numpy as np
+import numpy as np  
 
 class MeuAmbiente(gym.Env):
     ID_AGENTE = 2
@@ -16,7 +16,7 @@ class MeuAmbiente(gym.Env):
         self.action_space = gym.spaces.Discrete(4)
         self.observation_space = gym.spaces.Dict(
             {
-                "posicao": gym.spaces.Box(-10, 10, shape=(2,), dtype=float),
+                "posicao": gym.spaces.Box(0, 4, shape=(2,), dtype=float),
                 "brilho": gym.spaces.Box(0, 1, shape=(1,), dtype=float),
                 "fedor": gym.spaces.Box(0, 1, shape=(1, ), dtype=float)
             })
@@ -24,8 +24,8 @@ class MeuAmbiente(gym.Env):
 
 
     def validaPos(self, pos):
-        return (pos[0] >= 0 and pos[0] <= self.size and
-                    pos[1] >= 0 and pos[1] <= self.size)
+        return (pos[0] >= 0 and pos[0] < self.size and
+                    pos[1] >= 0 and pos[1] < self.size)
 
     def temFedor(self, p=None):
         if p is None:
@@ -49,26 +49,27 @@ class MeuAmbiente(gym.Env):
             (self.validaPos( (x, y+1) ) and self.grade[x][y+1] == MeuAmbiente.ID_POCO)
             )
 
-    def reset(self):
+    def reset(self, seed=None, **options):
         size = self.size
         self.grade = []
+        self.seed = seed
         for line in range(size):
             self.grade.append([MeuAmbiente.ID_PISO]*4)
         self.grade[0][0] = MeuAmbiente.ID_AGENTE
         self.grade[-1][-1] = MeuAmbiente.ID_WUMPUS
         self.grade[2][2] = MeuAmbiente.ID_POCO
         self.pos_agente = (0, 0)
-        self.abjetivo = (size-1, 0)
-        return {'posicao': np.array(self.pos_agente, dtype=np.float32), 
-                'brilho': 1 if self.temBrisa() else 0, 
-                'fedor': 1 if self.temFedor() else 0 }, {}
+        self.objetivo = (size-1, 0)
+        return {'posicao': np.array(self.pos_agente, dtype=float), 
+                'brilho': np.array([1] if self.temBrisa() else [0], dtype=float), 
+                'fedor': np.array([1] if self.temFedor() else [0], dtype=float) }, {}
 
     def step(self, acao):
         p = self.pos_agente
         if acao == MeuAmbiente.ACAO_DIR:
             prox_linha = p[0] + 1
             if prox_linha < self.size:
-                self.grade[p[0], p[1]] = MeuAmbiente.ID_PISO
+                self.grade[p[0]][p[1]] = MeuAmbiente.ID_PISO
                 self.grade[p[0] + 1][p[1]] = MeuAmbiente.ID_AGENTE
                 self.pos_agente = (prox_linha, p[1])
         elif acao == MeuAmbiente.ACAO_ESQ:
@@ -90,19 +91,21 @@ class MeuAmbiente(gym.Env):
                 self.grade[p[0]][p[1] - 1] = MeuAmbiente.ID_AGENTE
                 self.pos_agente = (p[0], prox_col)
         recompensa = 0
-        np = self.pos_agente #nova posicao
+        nps = self.pos_agente #nova posicao
+        fim = False
         if self.pos_agente == self.objetivo:
             recompensa = 0
-            done = True #agente alcançou o alvo
-        elif (self.grade[np[0]][np[1]] == MeuAmbiente.ID_WUMPUS or
-                self.grade[np[0]][np[1]] == MeuAmbiente.ID_POCO):
+            fim = True #agente alcançou o alvo
+        elif (self.grade[nps[0]][nps[1]] == MeuAmbiente.ID_WUMPUS or
+                self.grade[nps[0]][nps[1]] == MeuAmbiente.ID_POCO):
             recompensa = -1
-            done = True #agente morreu
-        return  ({'posicao': np, 
-                    'brilho': 1 if self.temBrisa() else 0, 
-                    'fedor' : 1 if self.temFedor() else 0}, 
+            fim = True #agente morreu
+        return  ({'posicao': np.array(self.pos_agente, dtype=float), 
+                 'brilho': np.array([1] if self.temBrisa() else [0], dtype=float), 
+                 'fedor': np.array([1] if self.temFedor() else [0], dtype=float) }, 
                     recompensa, 
-                    done, 
+                    np.bool_(fim), 
+                    np.bool_(False),
                     {})
 
     def render(self):
